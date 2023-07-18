7015da580e777e29ff801930194cddb38c16bd4d
marco bonaccorso <m.bonaccorso@consoftinformatica.it>
2023-07-18 11:13:30 +0200

    Update AuthenticationController.java

--- src/main/java/it/sincrono/controller/AuthenticationController.java
+++ src/main/java/it/sincrono/controller/AuthenticationController.java
@@ -2,6 +2,7 @@ package it.sincrono.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import it.sincrono.services.exceptions.ServiceException;
 
 @RestController
 @RequestMapping("/login-service")
+@CrossOrigin("*")	
 public class AuthenticationController {
 
 	@Autowired
