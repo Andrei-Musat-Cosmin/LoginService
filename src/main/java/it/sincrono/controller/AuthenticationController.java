6e93c6b0641e68d44ac933d16ad0efe64e6d9fda
aandreii02 <121028501+Andrei-Musat-Cosmin@users.noreply.github.com>
2023-07-19 16:22:56 +0200

    19072023_recupero password e cambio password

--- src/main/java/it/sincrono/controller/AuthenticationController.java
+++ src/main/java/it/sincrono/controller/AuthenticationController.java
@@ -10,19 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import it.sincrono.beans.Esito;
 import it.sincrono.requests.AuthenticationRequest;
+import it.sincrono.requests.EmailRequest;
 import it.sincrono.requests.RegisterRequest;
 import it.sincrono.responses.AuthenticationResponse;
 import it.sincrono.responses.GenericResponse;
 import it.sincrono.services.AuthenticationService;
 import it.sincrono.services.exceptions.ServiceException;
+import it.sincrono.services.utils.RestClient;
 
 @RestController
 @RequestMapping("/login-service")
-@CrossOrigin("*")	
+@CrossOrigin("*")
 public class AuthenticationController {
 
 	@Autowired
 	private AuthenticationService authenticationService;
+
+	@Autowired
+	private RestClient restClient;
 
 	@PostMapping("/authenticate")
 	public HttpEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
@@ -49,25 +54,37 @@ public class AuthenticationController {
 
 	}
 
-	@PostMapping("/cambia-password")
+	@PostMapping("/recupero-password")
 	public HttpEntity<GenericResponse> recuperoPassword(@RequestBody RegisterRequest request) {
 
 		HttpEntity<GenericResponse> httpEntity = null;
 
 		GenericResponse genericResponse = new GenericResponse();
-
+		EmailRequest emailRequest = new EmailRequest(null, null, null, null, null);
 		try {
 
 			System.out.println("Start invocation of method authenticate of Authentication Service");
 
-			authenticationService.recuperoPassword(request.getUsername());
+			String jwtToken = authenticationService.recuperoPassword(request.getUsername());
 
 			System.out.println("End invocation of method authenticate of Authentication Service");
 
 			httpEntity = new HttpEntity<GenericResponse>(genericResponse);
 
+			emailRequest.setTo(request.getUsername());
+			emailRequest.setSubject("Link per il recupero password");
+			emailRequest.setBody("http://localhost:4200/recupero-password/" + jwtToken
+					+ " Se non sei stato te a richiedere il reset della password,"
+					+ " per favore ignora questa email");
+
+			System.out.println(emailRequest);
+			restClient.sendRequest("http://localhost:8085/mail/send", "POST", emailRequest.toString());
+
 		} catch (ServiceException e) {
 			genericResponse.setEsito(new Esito(e.getCode(), e.getMessage(), null));
+			httpEntity = new HttpEntity<GenericResponse>(genericResponse);
+		} catch (Exception e) {
+			genericResponse.setEsito(new Esito(500, e.getMessage(), null));
 			httpEntity = new HttpEntity<GenericResponse>(genericResponse);
 		}
 		return httpEntity;
--- src/main/java/it/sincrono/services/utils/RestClient.java
+++ src/main/java/it/sincrono/services/utils/RestClient.java
@@ -1,4 +1,5 @@
 package it.sincrono.services.utils;
+
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.core.Response;
@@ -8,166 +9,115 @@ import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 import org.springframework.stereotype.Component;
 
+import it.sincrono.responses.AnagraficaDtoListResponse;
 import it.sincrono.responses.AnagraficaDtoResponse;
+import it.sincrono.responses.CommessaDtoListResponse;
+import it.sincrono.responses.FunzioniListResponse;
 import it.sincrono.responses.GenericResponse;
-
-import it.sincrono.responses.*;
-
-import it.sincrono.entity.TipoAzienda;
-
+import it.sincrono.responses.OperazioniListResponse;
+import it.sincrono.responses.OrganicoDtoListResponse;
+import it.sincrono.responses.TipologicheListResponse;
 
 @Component
 public class RestClient {
-    private ResteasyClient client;
+	private ResteasyClient client;
 
-    public RestClient() {
-        client = new ResteasyClientBuilder().build();
-    }
-
-    public GenericResponse sendRequest(String url, String method, String requestEntity) {
-        ResteasyWebTarget target = client.target(url);
-
-        Invocation.Builder builder = target.request();
-        
-        Response response;
-        switch (method.toUpperCase()) {
-            case "GET":
-                response = builder.get();
-                break;
-            case "POST":
-                response = builder.post(Entity.json(requestEntity));
-                break;
-            case "PUT":
-                response = builder.put(Entity.json(requestEntity));
-                break;
-            case "DELETE":
-                response = builder.delete();
-                break;
-            default:
-                throw new IllegalArgumentException("Invalid HTTP method: " + method);
-        }
-
-        if (response.getStatusInfo().getFamily() != Response.Status.Family.SUCCESSFUL) {
-            throw new RuntimeException("HTTP error code: " + response.getStatus());
-        }
-        
-        System.out.println("response: "+response.getEntity().getClass());
-        
-        //System.out.println(" tag: "+response.getEntityTag().getClass());
-
-
-        GenericResponse responseBody = readEntity(response,method,url);
-        response.close();
-
-        return responseBody;
-    }
-    
-    
-    private GenericResponse readEntity(Response response,String method,String url) {
-    	
-    	GenericResponse genericResponse = null;
-    	
-    	if(method=="POST" || method=="PUT" || method=="DELETE") {
-    		
-    		genericResponse=response.readEntity(GenericResponse.class);
-    		
-    	}else {
-    		
-    		if(url.contains("dettaglio-anagrafica")) {
-    			
-    			genericResponse=response.readEntity(AnagraficaDtoResponse.class);
-    			
-    		}else {
-    			
-    			if(url.contains("anagraficaDto-list")) {
-    				
-    				genericResponse=response.readEntity(AnagraficaDtoListResponse.class);
-    				
-    			}else {
-    				
-    				if(url.contains("dashboard")) {
-        				
-        				genericResponse=response.readEntity(CommessaDtoListResponse.class);
-        				
-        			}else {
-        				
-        				if(url.contains("organico ")) {
-            				
-            				genericResponse=response.readEntity(OrganicoDtoListResponse.class);
-            				
-            			}else {
-            				
-            				
-            				if(url.contains("tipo-azienda/map")) {
-                				
-                				genericResponse=response.readEntity(TipologicheListResponse.class);
-                				
-                			}else {
-                				
-                				if(url.contains("tipo-contratto/map")) {
-                    				
-                    				genericResponse=response.readEntity(TipologicheListResponse.class);
-                    			
-                    			}else {
-                    				
-                    				if(url.contains("tipo-contratto-nazionale/map")) {
-                        				
-                        				genericResponse=response.readEntity(TipologicheListResponse.class);
-                        				
-                        			}else {
-                        				
-                        				if(url.contains("tipo-livelli-contrattuali/map")) {
-                            				
-                            				genericResponse=response.readEntity(TipologicheListResponse.class);
-                            				
-                            			}else {
-                            				
-                            				if(url.contains("operazioni")) {
-                                				
-                                				genericResponse=response.readEntity(OperazioniListResponse.class);
-                            				
-                            				
-                            			}else {
-                            				
-                            					if(url.contains("funzioni-ruolo")) {
-                                				
-                            						genericResponse=response.readEntity(FunzioniListResponse.class);
-                            				
-                            				
-                            			}
-                            				
-                            				
-                            			}
-                        				
-                        			}
-                    				
-                    				
-                    			}
-                				
-                			}
-            				
-            				
-            			}
-        				
-        				
-        			}
-    				
-    			}
-    			
-    		}
-    	}
-    		
+	public RestClient() {
+		client = new ResteasyClientBuilder().build();
 	}
-    	
-	return genericResponse;
-    	
-    	 
-    	
-    	
-    	
-    }
 
-    public void close() {
-        client.close();
-    }
+	public GenericResponse sendRequest(String url, String method, String requestEntity) {
+		ResteasyWebTarget target = client.target(url);
+
+		Invocation.Builder builder = target.request();
+
+		Response response;
+		switch (method.toUpperCase()) {
+		case "GET":
+			response = builder.get();
+			break;
+		case "POST":
+			response = builder.post(Entity.json(requestEntity));
+			break;
+		case "PUT":
+			response = builder.put(Entity.json(requestEntity));
+			break;
+		case "DELETE":
+			response = builder.delete();
+			break;
+		default:
+			throw new IllegalArgumentException("Invalid HTTP method: " + method);
+		}
+
+		if (response.getStatusInfo().getFamily() != Response.Status.Family.SUCCESSFUL) {
+			throw new RuntimeException("HTTP error code: " + response.getStatus());
+		}
+
+		System.out.println("response: " + response.getEntity().getClass());
+
+		// System.out.println(" tag: "+response.getEntityTag().getClass());
+
+		GenericResponse responseBody = readEntity(response, method, url);
+		response.close();
+
+		return responseBody;
+	}
+
+	private GenericResponse readEntity(Response response, String method, String url) {
+
+		GenericResponse genericResponse = null;
+
+		if (method == "POST" || method == "PUT" || method == "DELETE") {
+
+			genericResponse = response.readEntity(GenericResponse.class);
+
+		} else if (url.contains("dettaglio-anagrafica") || url.contains("dettaglio-anagrafica-token")) {
+
+			genericResponse = response.readEntity(AnagraficaDtoResponse.class);
+
+		} else if (url.contains("anagraficaDto-list")) {
+
+			genericResponse = response.readEntity(AnagraficaDtoListResponse.class);
+
+		} else if (url.contains("dashboard")) {
+
+			genericResponse = response.readEntity(CommessaDtoListResponse.class);
+
+		} else if (url.contains("organico ")) {
+
+			genericResponse = response.readEntity(OrganicoDtoListResponse.class);
+
+		} else if (url.contains("tipo-azienda/map")) {
+
+			genericResponse = response.readEntity(TipologicheListResponse.class);
+
+		} else if (url.contains("tipo-contratto/map")) {
+
+			genericResponse = response.readEntity(TipologicheListResponse.class);
+
+		} else if (url.contains("tipo-contratto-nazionale/map")) {
+
+			genericResponse = response.readEntity(TipologicheListResponse.class);
+
+		} else if (url.contains("tipo-livelli-contrattuali/map")) {
+
+			genericResponse = response.readEntity(TipologicheListResponse.class);
+
+		} else if (url.contains("operazioni")) {
+
+			genericResponse = response.readEntity(OperazioniListResponse.class);
+
+		} else if (url.contains("funzioni-ruolo")) {
+
+			genericResponse = response.readEntity(FunzioniListResponse.class);
+
+		}
+
+		return genericResponse;
+
+	}
+
+	public void close() {
+		client.close();
+	}
 }
