9cdc389e5c399d7682146c9208217d799d05bfcb
aandreii02 <andreicosmin.musat02@gmail.com>
2023-10-10 11:58:07 +0200

    10102023_risolto rapportino

--- src/main/java/it/sincrono/repositories/dto/RapportinoDto.java
+++ src/main/java/it/sincrono/repositories/dto/RapportinoDto.java
@@ -5,16 +5,27 @@ import it.sincrono.entity.Anagrafica;
 public class RapportinoDto {
 
 	private MeseDto mese;
+	private String note;
 	private Anagrafica anagrafica;
-
-	public RapportinoDto(MeseDto mese, Anagrafica anagrafica) {
-		super();
-		this.mese = mese;
-		this.anagrafica = anagrafica;
-	}
+	private Integer giorniUtili;
+	private Double giorniLavorati;
+	private Integer annoRequest;
+	private Integer meseRequest;
 
 	public RapportinoDto() {
 		super();
+	}
+
+	public RapportinoDto(MeseDto mese, String note, Anagrafica anagrafica, Integer giorniUtili, Double giorniLavorati,
+			Integer annoRequest, Integer meseRequest) {
+		super();
+		this.mese = mese;
+		this.note = note;
+		this.anagrafica = anagrafica;
+		this.giorniUtili = giorniUtili;
+		this.giorniLavorati = giorniLavorati;
+		this.annoRequest = annoRequest;
+		this.meseRequest = meseRequest;
 	}
 
 	public MeseDto getMese() {
@@ -25,12 +36,52 @@ public class RapportinoDto {
 		this.mese = mese;
 	}
 
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
 	public Anagrafica getAnagrafica() {
 		return anagrafica;
 	}
 
 	public void setAnagrafica(Anagrafica anagrafica) {
 		this.anagrafica = anagrafica;
+	}
+
+	public Integer getGiorniUtili() {
+		return giorniUtili;
+	}
+
+	public void setGiorniUtili(Integer giorniUtili) {
+		this.giorniUtili = giorniUtili;
+	}
+
+	public Double getGiorniLavorati() {
+		return giorniLavorati;
+	}
+
+	public void setGiorniLavorati(Double giorniLavorati) {
+		this.giorniLavorati = giorniLavorati;
+	}
+
+	public Integer getAnnoRequest() {
+		return annoRequest;
+	}
+
+	public void setAnnoRequest(Integer annoRequest) {
+		this.annoRequest = annoRequest;
+	}
+
+	public Integer getMeseRequest() {
+		return meseRequest;
+	}
+
+	public void setMeseRequest(Integer meseRequest) {
+		this.meseRequest = meseRequest;
 	}
 
 }
