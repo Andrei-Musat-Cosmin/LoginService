31a997f826119d5a5e82a1ee89c59b3b599a6135
aandreii02 <andreicosmin.musat02@gmail.com>
2023-10-20 12:29:43 +0200

    update login con i cambiamenti alle classi dto

--- src/main/java/it/sincrono/repositories/dto/GiornoDto.java
+++ src/main/java/it/sincrono/repositories/dto/GiornoDto.java
@@ -10,14 +10,31 @@ public class GiornoDto {
 
 	private List<Double> oreOrdinarie;
 
-	public GiornoDto(Integer giorno, List<String> cliente, List<Double> oreOrdinarie) {
+	private List<StraordinarioDto> straordinari;
+
+	private Boolean ferie;
+
+	private Boolean malattie;
+
+	private Double permessi;
+
+	private String note;
+
+	public GiornoDto(Integer giorno, List<String> cliente, List<Double> oreOrdinarie,
+			List<StraordinarioDto> straordinari, Boolean ferie, Boolean malattie, Double permessi, String note) {
 		super();
 		this.giorno = giorno;
 		this.cliente = cliente;
 		this.oreOrdinarie = oreOrdinarie;
+		this.straordinari = straordinari;
+		this.ferie = ferie;
+		this.malattie = malattie;
+		this.permessi = permessi;
+		this.note = note;
 	}
 
 	public GiornoDto() {
+		super();
 	}
 
 	public Integer getGiorno() {
@@ -43,5 +60,49 @@ public class GiornoDto {
 	public void setOreOrdinarie(List<Double> oreOrdinarie) {
 		this.oreOrdinarie = oreOrdinarie;
 	}
+
+	public List<StraordinarioDto> getStraordinari() {
+		return straordinari;
+	}
+
+	public void setStraordinari(List<StraordinarioDto> straordinari) {
+		this.straordinari = straordinari;
+	}
+
+	public Boolean getFerie() {
+		return ferie;
+	}
+
+	public void setFerie(Boolean ferie) {
+		this.ferie = ferie;
+	}
+
+	public Boolean getMalattie() {
+		return malattie;
+	}
+
+	public void setMalattie(Boolean malattie) {
+		this.malattie = malattie;
+	}
+
+	public Double getPermessi() {
+		return permessi;
+	}
+
+	public void setPermessi(Double permessi) {
+		this.permessi = permessi;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+	
+	
+	
 
 }
