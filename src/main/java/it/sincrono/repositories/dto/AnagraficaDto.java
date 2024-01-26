f464ec166ed3b04c19d1282181abe2affc7fe61b
Ruamez <ruamepnn@gmail.com>
2024-01-26 15:11:00 +0100

    AggiuntoCampoAnagraficaDtoResidenzaDomicilioUguali

--- src/main/java/it/sincrono/repositories/dto/AnagraficaDto.java
+++ src/main/java/it/sincrono/repositories/dto/AnagraficaDto.java
@@ -17,16 +17,28 @@ public class AnagraficaDto {
 
 	private Ruolo ruolo;
 
-	public AnagraficaDto(Anagrafica anagrafica, Contratto contratto, List<Commessa> commesse, Ruolo ruolo) {
+	private Boolean residenzaDomicilioUguali;
+
+	public AnagraficaDto(Anagrafica anagrafica, Contratto contratto, List<Commessa> commesse, Ruolo ruolo,
+			Boolean residenzaDomicilioUguali) {
 		super();
 		this.anagrafica = anagrafica;
 		this.contratto = contratto;
 		this.commesse = commesse;
 		this.ruolo = ruolo;
+		this.residenzaDomicilioUguali = residenzaDomicilioUguali;
 	}
 
 	public AnagraficaDto() {
 		super();
+	}
+
+	public Boolean getResidenzaDomicilioUguali() {
+		return residenzaDomicilioUguali;
+	}
+
+	public void setResidenzaDomicilioUguali(Boolean residenzaDomicilioUguali) {
+		this.residenzaDomicilioUguali = residenzaDomicilioUguali;
 	}
 
 	public Anagrafica getAnagrafica() {
